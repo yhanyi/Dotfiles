030ff70d9e0aaf2af2340d6847f8eb09b66ea8bf
yhanyi <yeohhanyi0916@gmail.com>
2025-03-31 00:04:11 +0800

    feat: add vim-options

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -1,7 +1,2 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-vim.opt.clipboard = "unnamedplus"
 require("config.lazy")
+require("vim-options")
