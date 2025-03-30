6d3798d2ab378e07395c72ebc95663754c4a2236
yhanyi <yeohhanyi0916@gmail.com>
2025-03-31 01:19:39 +0800

    feat: update lspconfigs

--- config/.config/nvim/lua/plugins/lsp-config.lua
+++ config/.config/nvim/lua/plugins/lsp-config.lua
@@ -20,8 +20,105 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      vim.diagnostic.config({
+        virtual_text = {
+          prefix = '●',
+          source = "if_many",
+        },
+        float = {
+          source = "always",
+          border = "rounded",
+          header = "",
+          prefix = "",
+        },
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+      })
+
+      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, { desc = 'Show diagnostic explanation' })
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous diagnostic' })
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next diagnostic' })
+      vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, { desc = 'List all diagnostics' })
+
       local lspconfig = require("lspconfig")
+
       lspconfig.lua_ls.setup({})
+
+      lspconfig.ts_ls.setup({})
+
+      lspconfig.rust_analyzer.setup({
+        settings = {
+          ['rust-analyzer'] = {
+            diagnostics = {
+              enable = false;
+            }
+          }
+        }
+      })
+
+      lspconfig.clangd.setup {
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders",
+          "--fallback-style=llvm"
+        },
+        root_dir = lspconfig.util.root_pattern(
+          '.clangd',
+          '.clang-tidy',
+          '.clang-format',
+          'compile_commands.json',
+          'compile_flags.txt',
+          'configure.ac',
+          '.git'
+        ),
+        init_options = {
+          usePlaceholders = true,
+          completeUnimported = true,
+          clangdFileStatus = true
+        },
+      }
+
+      lspconfig.cmake.setup(
+        {
+          filetypes = {"CMakeLists.txt"},
+          root_dir = lspconfig.util.root_pattern("CMakeLists.txt", ".git"),
+        }
+      )
+
+      lspconfig.pyright.setup({
+        settings = {
+          python = {
+            analysis = {
+              typeCheckingMode = "basic",
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+            },
+          },
+        },
+      })
+
+      lspconfig.gopls.setup({
+        cmd = {"gopls"},
+        filetypes = {"go", "gomod", "gowork", "gotmpl"},
+        root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
+            usePlaceholders = true,
+            completeUnimported = true,
+          },
+        },
+      })
+
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
