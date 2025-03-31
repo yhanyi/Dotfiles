375216cefa28eec78acf3fa5e2f34728cf591125
yhanyi <yeohhanyi0916@gmail.com>
2025-03-31 12:40:16 +0800

    feat: update cord

--- config/.config/nvim/lua/plugins/cord.lua
+++ config/.config/nvim/lua/plugins/cord.lua
@@ -4,6 +4,23 @@ return {
     build = ':Cord update',
     config = function()
       local config = require("cord")
+
+      -- Custom language handlers
+      local handlers = {
+        ["cpp"] = function(opts)
+          return "Optimising " .. opts.filename
+        end,
+        ["rust"] = function(opts)
+          return "Oxidising " .. opts.filename
+        end,
+        ["go"] = function(opts)
+          return "Going " .. opts.filename
+        end,
+        ["python"] = function(opts)
+          return "Snaking " .. opts.filename
+        end,
+      }
+
       config.setup({
         usercmds = true,           -- Enable user commands
         log_level = 'error',       -- One of 'trace', 'debug', 'info', 'warn', 'error', 'off'
@@ -18,6 +35,8 @@ return {
           tooltip = 'The Superior Text Editor', -- Text to display when hovering over the editor's image
         },
         display = {
+          theme = "catppuccin",
+          flavor = "accent",
           show_time = true,             -- Display start timestamp
           show_repository = true,       -- Display 'View repository' button linked to repository url, if any
           show_cursor_position = false, -- Display line and column number of cursor's position
@@ -26,9 +45,9 @@ return {
           workspace_blacklist = {},     -- List of workspace names that will hide rich presence
         },
         lsp = {
-          show_problem_count = false, -- Display number of diagnostics problems
-          severity = 1,               -- 1 = Error, 2 = Warning, 3 = Info, 4 = Hint
-          scope = 'workspace',        -- buffer or workspace
+          show_problem_count = true, -- Display number of diagnostics problems
+          severity = 1,              -- 1 = Error, 2 = Warning, 3 = Info, 4 = Hint
+          scope = 'workspace',       -- buffer or workspace
         },
         idle = {
           enable = true, -- Enable idle status
@@ -39,34 +58,26 @@ return {
           tooltip = '💤', -- Text to display when hovering over the idle image
         },
         text = {
-          viewing = 'Viewing {}',                    -- Text to display when viewing a readonly file
-          editing = 'Editing {}',                    -- Text to display when editing a file
-          file_browser = 'Browsing files in {}',     -- Text to display when browsing files (Empty string to disable)
-          plugin_manager = 'Managing plugins in {}', -- Text to display when managing plugins (Empty string to disable)
-          lsp_manager = 'Configuring LSP in {}',     -- Text to display when managing LSP servers (Empty string to disable)
-          vcs = 'Committing changes in {}',          -- Text to display when using Git or Git-related plugin (Empty string to disable)
-          workspace = 'In {}',                       -- Text to display when in a workspace (Empty string to disable)
+          viewing = function(opts)
+            return ('Viewing ' .. opts.filename)
+          end,
+          editing = function(opts)
+            return handlers[opts.filetype] and handlers[opts.filetype](opts) or ('Editing ' .. opts.filename)
+          end,
+          file_browser = function(opts)
+            return ('Browsing files in ' .. opts.workspace)
+          end,
+          workspace = function(opts)
+            return ('In ' .. opts.workspace)
+          end,
         },
-        -- buttons = {
-        --   {
-        --     label = 'View Repository', -- Text displayed on the button
-        --     -- url = 'git',               -- URL where the button leads to ('git' = automatically fetch Git repository URL)
-        --   },
-        --   {
-        --     label = 'View Plugin',
-        --     url = 'https://github.com/vyfor/cord.nvim',
-        --   }
-        -- },
-        -- assets = nil, -- Custom file icons, see the wiki*
-        -- assets = {
-        --   lazy = {                                 -- Vim filetype or file name or file extension = table or string
-        --     name = 'Lazy',                         -- Optional override for the icon name, redundant for language types
-        --     icon = 'https://example.com/lazy.png', -- Rich Presence asset name or URL
-        --     tooltip = 'lazy.nvim',                 -- Text to display when hovering over the icon
-        --     type = 'plugin_manager',               -- One of 'language', 'file_browser', 'plugin_manager', 'lsp_manager', 'vcs' or respective ordinals; defaults to 'language'
-        --   },
-        --   ['Cargo.toml'] = 'crates',
-        -- },
+
+        assets = {
+          ['Cargo.toml'] = { text = 'Managing Cargo dependencies...' },
+          ['CMakeLists.txt'] = { text = 'Configuring build system...' },
+          ['Dockerfile'] = { text = 'Containerising applications...' },
+          ['docker-compose.yml'] = { text = 'Orchestrating containers...' },
+        },
       })
     end
   }
