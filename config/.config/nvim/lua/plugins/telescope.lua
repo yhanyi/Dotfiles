039163e8115995de0afb20e4b18fb78473e0133d
yhanyi <yeohhanyi0916@gmail.com>
2025-03-31 22:00:35 +0800

    feat: add noice

--- config/.config/nvim/lua/plugins/telescope.lua
+++ config/.config/nvim/lua/plugins/telescope.lua
@@ -55,7 +55,7 @@ return {
           },
         },
         pickers = {},
-        extensions = {},
+        extensions = { "themes", "terms", "noice" },
       })
       vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'Find files' })
       vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { desc = 'Find text' })
